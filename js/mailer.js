f6ce534904637115d8e2cc920300d50a9024dad8
Volan Nnanpalle <vnnanpalle3@gatech.edu>
2023-12-24 23:28:12 -0500

    fixed email issue in local env

--- js/mailer.js
+++ js/mailer.js
@@ -1,27 +1,34 @@
 /*
-using Email JS for sending emails
+using Elastic email JS for sending emails
 
 Author: Volan Nnanpalle
 */
 
 
 function sendEmail() {
-    var params = {
-        name: document.getElementById('volunteer_name').value,
-        email: document.getElementById('volunteer_email').value,
-        message: document.getElementById('volunteer_message').value
-    };
-    const serviceID = window.config.emailjs_service_id;
-    const templateID = window.config.emailjs_template_id;
-    emailjs.send(serviceID, templateID, params)
-        .then(
+    let volunteer_email = document.getElementById('volunteer_email').value
+    let volunteer_name = document.getElementById('volunteer_name').value
+    let subject = 'New Volunteer Request message from NNACOR Website from: ' + volunteer_name
+    let volunteer_message = document.getElementById('volunteer_message').value
 
-            res => {
-                document.getElementById('volunteer_name').value
-                document.getElementById('volunteer_email').value
-                document.getElementById('volunteer_message').value
-                alert("Email sent!")
-            })
-        .catch((err) => console.log(err));
-    
+    let body = `
+    Hello,<br><br>
+
+    <h4>Volunteer's Name: </h4>${volunteer_name}
+    <br>
+    <h4>Volunteer's Email: </h4>${volunteer_email}
+    <br>
+    <h4>Volunteer's Message: </h4>${volunteer_message}
+    `;
+
+    Email.send({
+        SecureToken: "7f2bb0a1-4e31-4eeb-b429-a4fbd51dbca5",
+        To: 'nnacorarayoflight@gmail.com',
+        From: 'nnacorarayoflight@gmail.com',
+        Subject: subject,
+        Body: body
+    }).then(
+        message => alert('Email Sent!')
+    );
+
 }
